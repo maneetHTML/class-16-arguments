02633c9981652e812e15643bb6b25806f859254b
maneetHTML <maneet.harmeet@gmail.com>
2025-03-06 17:16:30 +1300

    class 16

---
+++ activity1.py
@@ -0,0 +1,7 @@
+def greeting (name,msg1="welcome",msg2="have a great day"):
+    """THIS IS """
+    print(msg1,name)
+    print(msg2,name)
+na=input("enter your name : ")
+greeting(na)
+print(greeting.__doc__)
---
+++ activity2.py
@@ -0,0 +1,8 @@
+def fun(a,m,d,c):
+    print(a-m/c*d)
+
+g=6
+h=2
+j=9
+w=8
+fun(j,w,h,g)
